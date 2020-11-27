34272239bd9d5b8c03a0aa8f45de70164d91ba79
jpinjuh <josip.pinjuh19@gmail.com>
2020-11-28 00:28:12 +0100

    initial

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-main>
+      <v-container>
+        <v-fade-transition mode="out-in">
+          <router-view></router-view>
+        </v-fade-transition>
+      </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+
+  data: () => ({
+    //
+  }),
+}
+</script>
